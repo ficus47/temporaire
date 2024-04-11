6c823473572c0d593bb59b19b8b843bcbe0e0a32
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-04-11 19:38:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import streamlit as st
 st.title("Exposée de Joseph")
 st.balloons()
 
-a = open("moi.docx", "rb").read()
+a = open("Joseph.txt", "rb").read()
 
 st.download_button("telecharger l'explosée", data=a, file_name="Joseph.txt", mime="text/plain")
